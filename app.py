01519d21be01a835deb8249395d6da2414d03dbc
Radu Cojan <cojanradu@yahoo.com>
2020-04-13 10:15:34 +0100

    added option for answers as checkboxes

--- app.py
+++ app.py
@@ -37,17 +37,29 @@ def make_exercise(action, user):
     a = use_nr[a_temp]
     b = random.randint(1, 12)
 
-    hint = a
+    hint = int(a)
+    keep_b = int(b)
 
     if user['mult_opt_can_be_inverted']:
         x = random.randint(0, 1)
         if x:
-            c = b
-            b = a
-            a = c
+            a, b = b, a
 
     c = int(a)*int(b)
-    mult_tuple = [int(a), int(b), c, int(hint)] if action == 'multiply' else [4, 5, 6]
+
+    """ generate options """
+    if user['mult_opt_answer_type'] > 1:
+        rand_range =  2*user['mult_opt_answer_type']
+        range_low = hint - random.randint(1,rand_range)
+        low = range_low if range_low > 0 else 1
+        options = random.sample(range(low, low+rand_range), user['mult_opt_answer_type'])
+        if not (keep_b in options):
+            options.pop(0)
+            options.insert(0, keep_b)
+            random.shuffle(options)
+        new_list = [x*hint for x in options]
+
+    mult_tuple = [int(a), int(b), c, hint, new_list] if action == 'multiply' else [4, 5, 6]
     return mult_tuple
 
 
